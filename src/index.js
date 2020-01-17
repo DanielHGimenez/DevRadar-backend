8c0b022ed1562e402820565080184aeccee3e9e1
DanielHGimenez <danielhgimenez027@gmail.com>
2020-01-17 16:03:46 -0300

    inclusão de cors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 const routes = require('./routes');
+const cors = require('cors');
 
 mongoose.connect('mongodb+srv://<usuario>:<senha>@cluster0-3kvrj.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
